50b2eb18032b9ee1f4c1122b7f59cda4c3fed8d7
Administrator <>
2018-08-17 06:50:57 +0000

    已删除 hn_med_deviceInfo

--- mtygl/hn_med_deviceInfo/js/device.js
+++
@@ -1,9 +0,0 @@
-require.config({
-    paths:{
-        "jquery":"../../../common/libs/jquery/jquery-1.11.3.min",
-        "bootstrap":"../../../common/libs/bootstrap/js/bootstrap.min",
-        "bootstrap-table":"../../../common/libs/bootstrap/js/bootstrap-table.js",
-        "bootstrap-treeview":"../../../common/libs/bootstrap/js/bootstrap-treeview.js",
-
-    }
-});
